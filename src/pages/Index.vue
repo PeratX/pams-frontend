41a722c718dd00889cabf84e610df3648c375721
peratx <peratx@itxtech.org>
2021-12-09 13:42:51 +0800

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">登录PAMS</div>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+  setup() {
+    return '';
+  }
+});
+</script>
